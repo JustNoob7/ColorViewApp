7f096a75bfc6cd8128e60203aba66a49b1eacd3b
Yaroslav Boyko <Spectraldagger322@gmail.com>
2021-10-06 20:41:40 +0300

    Initial Commit

---
+++ ColorViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorViewApp
+//
+//  Created by Ярослав Бойко on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
